62034d056f1bd0b206991c5dc66c4caed1c89c7d
Rachna-Mishra <rachu00786@gmail.com>
2020-06-15 15:28:45 +0530

    Add files via upload

---
+++ SpringDemo2/src/Tutorial/Test.java
@@ -0,0 +1,15 @@
+package Tutorial;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.*;
+
+    public class Test {
+        public static void main(String[] args) {
+            ApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+
+            Employee s=(Employee)context.getBean("e");
+            s.show();
+
+        }
+    }
