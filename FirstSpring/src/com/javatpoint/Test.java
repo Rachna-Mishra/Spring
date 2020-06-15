9582e0a65e4afac0358369fe44f62910876db183
Rachna-Mishra <rachu00786@gmail.com>
2020-06-15 14:54:48 +0530

    Add files via upload

---
+++ FirstSpring/src/com/javatpoint/Test.java
@@ -0,0 +1,18 @@
+package com.javatpoint;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+    public class Test {
+        public static void main(String[] args) {
+
+            ApplicationContext context =new ClassPathXmlApplicationContext("applicationContext.xml");
+
+            Student student=(Student)context.getBean("studentbean");
+            student.displayInfo();
+        }
+    }
+
+
